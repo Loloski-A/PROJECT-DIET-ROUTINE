a9115807103a2fc87b61c05a4b1d91370532ac91
SOLE LOLO ADAN <loloski17@gmail.com>
2023-06-03 12:07:38 +0300

    fifth commit

--- DIETROUTINE/app/src/main/java/com/example/diet_routine/Assessment_page.java
+++ DIETROUTINE/app/src/main/java/com/example/diet_routine/Assessment_page.java
@@ -2,13 +2,29 @@ package com.example.diet_routine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Assessment_page extends AppCompatActivity {
-
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessment_page);
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openresult_page();
+            }
+        });
+    }
+    public void openresult_page()
+    {
+        Intent intent =new Intent(this, Result_page.class);
+        startActivity(intent);
     }
 }
---
+++ DIETROUTINE/app/src/main/java/com/example/diet_routine/Result_page.java
@@ -0,0 +1,14 @@
+package com.example.diet_routine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Result_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_page);
+    }
+}
